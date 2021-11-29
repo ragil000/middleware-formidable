4b20f3ff5454023fb9165154127ca16e5ba3f148
ragil000 <ragilmanggalaning42@gmail.com>
2021-11-29 14:10:57 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+
+// const app = express();
+// const port = 3000;
+// const formdiable = require('./lib/middleware.js');
+
+// app.post('/', formdiable({ multiples: true }), (req, res) => {
+//   console.log('cc', req.fields);
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
